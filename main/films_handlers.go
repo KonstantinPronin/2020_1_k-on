932f0994805e164c74091935635efa5857458227
Oleg <elizarov1999@list.ru>
2020-02-24 19:38:21 +0300

    handlers with json (in/out)put

--- main/films_handlers.go
+++ main/films_handlers.go
@@ -1,9 +1,9 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/gorilla/mux"
-	"log"
 	"net/http"
 	"strconv"
 )
@@ -19,64 +19,47 @@ func createFilmHandler() *FilmHandler {
 }
 
 func (filmHandler *FilmHandler) getFilmsList(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	for _, val := range filmHandler.films.films {
-		fmt.Fprintf(w, "%d ", val.ID)
-		fmt.Fprintf(w, "%s ", val.Name)
-		fmt.Fprintf(w, "%d \n", val.YearCreated)
-
-	}
+	json.NewEncoder(w).Encode(filmHandler.films.films)
 	return
 }
 
 func (filmHandler *FilmHandler) getFilm(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
 	vars := mux.Vars(r)
 	id, err := strconv.Atoi(vars["id"])
 	if err != nil {
 		http.Error(w, `{"error":"bad id"}`, 400)
 		return
 	}
-	f, ok := filmHandler.films.GetById(uint(id))
+	film, ok := filmHandler.films.GetById(uint(id))
 	if !ok {
 		http.Error(w, `{"error":"not found this film"}`, 404)
 		return
 	} else {
-		fmt.Fprintf(w, "%d ", f.ID)
-		fmt.Fprintf(w, "%s ", f.Name)
-		fmt.Fprintf(w, "%d \n", f.YearCreated)
+		json.NewEncoder(w).Encode(film)
 	}
 }
 
 func (filmHandler *FilmHandler) createFilm(w http.ResponseWriter, r *http.Request) {
-	if err := r.ParseForm(); err != nil {
-		http.Error(w, "server error in parsing form", 500)
-		log.Fatalf("%s", err)
-
+	defer r.Body.Close()
+	decoder := json.NewDecoder(r.Body)
+	var film Film
+	err := decoder.Decode(&film)
+	if err != nil {
+		http.Error(w, `{"error":"can't parse json'"}`, 500)
+		fmt.Printf("%s", err)
 		return
 	}
-	name := r.FormValue("Name")
-	_, is := filmHandler.films.GetByName(name)
+	_, is := filmHandler.films.GetByName(film.Name)
 	if is {
 		fmt.Fprint(w, "film with this name already exists:")
 		return
 	}
-	year, err := strconv.Atoi(r.FormValue("Year"))
-	if err != nil {
-		fmt.Fprintf(w, "year must be int!: %s", err)
-		return
-	}
-	film := Film{
-		ID:          filmHandler.films.count + 1,
-		Name:        name,
-		YearCreated: year,
-	}
-
 	filmHandler.films.Add(&film)
 	ok := filmHandler.films.UpdateFilmList()
 	if ok {
-		http.Redirect(w, r, "/films/"+strconv.Itoa(int(film.ID))+"/", 301)
+		//http.Redirect(w, r, "/films/"+strconv.Itoa(int(film.ID))+"/", 301)
+		json.NewEncoder(w).Encode(film)
 	} else {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		http.Error(w, "can't update database", http.StatusInternalServerError)
 	}
 }
