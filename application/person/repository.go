d7ff44f897e191df71dbe43cf23a2d38a7050469
Konstantin Pronin <sayfer97@yandex.ru>
2020-04-05 16:26:20 +0300

    Changed person repository interface

--- application/person/repository.go
+++ application/person/repository.go
@@ -8,4 +8,6 @@ type Repository interface {
 	GetById(Id uint) (*models.Person, error)
 	Add(p *models.Person) (*models.Person, error)
 	Update(p *models.Person) (*models.Person, error)
+	GetActorsForFilm(filmId uint) (models.ListPersonArr, error)
+	GetActorsForSeries(seriesId uint) (models.ListPersonArr, error)
 }
