514004c5ba5cc5d158e102bd75b71f99dcb91117
Oleg <elizarov1999@list.ru>
2020-04-14 19:52:56 +0300

    Correct genre

--- application/series/repository/db_repository.go
+++ application/series/repository/db_repository.go
@@ -21,7 +21,7 @@ func NewPostgresForSeries(db *gorm.DB) series.Repository {
 func (p PostgresForSerials) GetSeriesGenres(fid uint) (models.Genres, bool) {
 	genres := &models.Genres{}
 	db := p.DB.Table("kinopoisk.genres").Select("genres.name,genres.reference").
-		Joins("join kinopoisk.series_genres on series_genres.genre_id=genres.id").
+		Joins("join kinopoisk.series_genres on series_genres.genre_ref=genres.reference").
 		Where("series_genres.series_id=?", fid).Order("genres.name").Find(genres)
 	err := db.Error
 	if err != nil {
